f9643d0eca25edbab196f91e828df8542744911c
Rakesh <rakeshvishwas730@gmail.com>
2025-11-08 13:49:26 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
             attribute="class"
             defaultTheme="system"
           >
-            <Navbar session={session}/>
+            {/* <Navbar session={session}/> */}
             {children}
             <Footer />
           </ThemeProvider>
