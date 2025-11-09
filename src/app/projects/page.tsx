08dd2f1b461d15ace2a810a7f5a03989c9ac0794
Rakesh <rakeshvishwas730@gmail.com>
2025-11-09 13:00:24 +0530

    Update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Project page</div>
+    <div className='flex h-screen font-xl text-bold w-full mt-100 item-center justify-center'>Show Project</div>
   )
 }
 
